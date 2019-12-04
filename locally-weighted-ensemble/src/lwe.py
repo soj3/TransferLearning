86705b16a65fd88a6530326cd282bbaf1b6445db
Kennan LeJeune <kc.lejeune@gmail.com>
2019-12-04 01:03:19 -0500

    add neighborhood graph comparison for p(M|x) estimation

--- locally-weighted-ensemble/src/lwe.py
+++ locally-weighted-ensemble/src/lwe.py
@@ -1,57 +1,101 @@
-from sklearn.cluster import AgglomerativeClustering
-from typing import List, Any, Dict, Tuple
-from sklearn.svm import LinearSVC
-import numpy as np
 from sklearn import metrics
+from sklearn.cluster import AgglomerativeClustering as Cluster
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model.base import LinearModel
+from sklearn.svm import LinearSVR
 from example import Example, SentimentExample
+from networkx import Graph
+import numpy as np
+from typing import List, Any, Dict, Tuple
 
 
 def lwe(
     train_sets: List[List[Example]],
-    models: List[object],
+    models: List[LinearModel],
     test_data: List[Example],
     threshold: float,
-    cluster_value: int = 2,
+    num_clusters: int = 2,
 ) -> List[any]:
+
     """
     Inputs:
+
         - train_data:
             list of k training sets D1, D2,..., Dk
-        - k classification models
+        - modesl:
             list of models M1, M2,..., Mk for k > 1
-        - Test set T from a different domain with the same task
-        - Threshold and cluster number c
+        - test_data:
+            test set from a different domain with the same task
+        - Threshold
+        - cluster number for localizing the test domain
     Outputs:
+
         - The set of predicted labels Y for examples in T
     """
-    cluster = AgglomerativeClustering()
+
+    # use a hierarchial clustering model to separate into 'positive' and 'negative' clusters
+    cluster = Cluster(n_clusters=num_clusters)
     predicted_clusters = [
-        pred
+        prediction
+        for prediction in cluster.fit_predict(train_data)
         for train_data in train_sets
-        for predictions in cluster.fit_predict(train_data)
-        for pred in predictions
     ]
 
-    # TODO extract the actual cluster/class values from
-    if purity([ex[-1] for ex in train_sets], predicted_clusters) > 0.5:
-        pass
 
-    for ex in test_data:
-        pass
+def s(gm: Graph, gt: Graph, x: Example):
 
-
-def generate_neighborhood():
     pass
+
+
+def generate_neighborhood(
+    train: List[Example], test: List[Example], model: LinearModel, num_clusters=2
+):
+    """
+    Implementation necessary for the proposed weight caculation in eq. 5 of Gao et. al
+
+    Inputs:
+
+        - train:
+            a single training set of examples
+        - test:
+            a single testing set of examples from a separate domain from train
+        - model:
+            a base model which has been trained on train to be evaluated on test and compared
+            with clustering results
+
+    Outputs:
+
+        - a tuple of the graphs (gm, gt) as used in eq. 5 for the model weight calculation
+    """
+    gt, gm = Graph(), Graph()
+    gm.add_nodes_from(test)
+    gt.add_nodes_from(test)
+
+    model.fit(train)
+    model_predictions = model.predict(test)
+
+    cluster = Cluster(n_clusters=num_clusters)
+    cluster_predictions = cluster.fit_predict(test)
+
+    preds = zip(test, model_predictions, cluster_predictions)
+    for u, m1, c1 in preds:
+        for v, m2, c2 in preds:
+            if u is not v:
+                # if the examples have the same predicted output from the model on the test set, add a connecting edge
+                if m1 == m2:
+                    gm.add_edge(u, v)
+                if c1 == c2:
+                    gt.add_edge(u, v)
+    return gm, gt
 
 
 def conditional_probability(w, x):
     pass
 
 
-def compute_prediction(y: List[Any], E, x):
+def compute_prediction(m: LinearModel, examples: List[Example]):
     # wM_i, x = P(M_i | x) is the true model weight that is locally adjusted for x representing the model's effectiveness on the test domain
-    pass
-
+    m.predict(examples)
     # return np.sum(np.prod(w[i]))
 
 
